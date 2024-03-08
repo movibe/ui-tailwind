28046803708711dc374703704252681602cf728d
Willian Ribeiro Angelo <agfoccus@gmail.com>
2024-03-07 23:40:31 -0300

    ci: build and lint

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,19 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import { terser } from "rollup-plugin-terser";
-import ttsc from "ttypescript";
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+import {terser} from 'rollup-plugin-terser'
+import ttsc from 'ttypescript'
 
 export default {
-  input: "src/index.ts", // Seu arquivo de entrada
+  input: 'src/index.ts', // Seu arquivo de entrada
   output: [
     {
-      file: "dist/bundle.cjs.js", // CommonJS
-      format: "cjs",
+      file: 'dist/bundle.cjs.js', // CommonJS
+      format: 'cjs',
     },
     {
-      file: "dist/bundle.esm.js", // ES Module
-      format: "es",
+      file: 'dist/bundle.esm.js', // ES Module
+      format: 'es',
       sourcemap: true,
     },
   ],
@@ -23,9 +23,9 @@ export default {
     typescript({
       // Using ttsc to use transformers from tsconfig and parse our absolut paths
       tsconfig: `tsconfig.build.json`,
-      typescript: ttsc, // It will transpile the src folder (but no change the file extension – see link: https://github.com/ezolenko/rollup-plugin-typescript2#rollup-plugin-babel)
+      typescript: ttsc,
     }),
     terser(), // Minifica o código para produção
   ],
-  external: ["react", "react-native"], // Lista de dependências externas que não devem ser incluídas no bundle
-};
+  external: ['react', 'react-native'], // Lista de dependências externas que não devem ser incluídas no bundle
+}
